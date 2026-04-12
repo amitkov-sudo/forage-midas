bd06e4b8641263653e13b4e3011b59e0d976b4dd
Alex Mitkov <mitkovaleks59@gmail.com>
2026-04-11 17:27:32 -0700

    Constructed Kafka listener class to consume transactions from configured topic

---
+++ src/test/java/com/jpmc/midascore/KafkaTransactionListener.java
@@ -0,0 +1,12 @@
+package com.jpmc.midascore;
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.stereotype.Component;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Component
+public class KafkaTransactionListener {
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
+    public void listen(Transaction transaction){
+        System.out.println("Received: " + transaction);
+    }
+}
