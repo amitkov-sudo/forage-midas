27058b2772f6166be1f573bbac9cb79764fae352
Alex Mitkov <mitkovaleks59@gmail.com>
2026-04-14 17:28:14 -0700

    Design a REST API Controller that tallies user balances

---
+++ src/main/java/com/jpmc/midascore/BalanceController.java
@@ -0,0 +1,23 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BalanceController {
+
+    private final UserRepository userRepository;
+
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> new Balance(user.getBalance()))
+                .orElse(new Balance(0.0f));
+    }
+}
